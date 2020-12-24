bc680f2f0ed356ffc151838a67f0448086066b16
Mat Sz <contact@matsz.dev>
2020-12-24 14:11:34 +0100

    added addbackground

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,43 @@
-import * as React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { addBackground, BackgroundControls } from 'addbackground';
+import { backgrounds } from 'addbackground/lib/backgrounds';
 
-export const Background = () => {
-  return <div></div>;
+export interface BackgroundProps {
+  type: keyof typeof backgrounds;
+  primaryColor?: string;
+  secondaryColor?: string;
+  backgroundColor?: string;
+  className?: string;
+}
+
+export const Background: React.FC<BackgroundProps> = ({
+  type,
+  primaryColor,
+  secondaryColor,
+  backgroundColor,
+  className
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const backgroundControlsRef = useRef<BackgroundControls>();
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      backgroundControlsRef.current = addBackground({
+        canvas: canvasRef.current,
+        type,
+        primaryColor,
+        secondaryColor,
+        backgroundColor
+      });
+    }
+
+    return () => {
+      if (backgroundControlsRef.current) {
+        backgroundControlsRef.current.stop();
+        backgroundControlsRef.current = undefined;
+      }
+    };
+  });
+
+  return <canvas ref={canvasRef} className={className}></canvas>;
 };
