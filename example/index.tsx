577a338307ba2fadce321ec18864c97ad0b54307
Mat Sz <contact@matsz.dev>
2020-12-25 14:03:32 +0100

    improved example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,26 @@
 import 'react-app-polyfill/ie11';
-import * as React from 'react';
+import React, { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 import { Background } from '../.';
+import { backgrounds } from 'addbackground/lib/backgrounds';
+
+const backgroundTypes = Object.keys(backgrounds);
 
 const App = () => {
+  const [backgroundType, setBackgroundType] = useState(backgroundTypes[0]);
+
   return (
-    <div style={{ minHeight: '400px', position: 'relative' }}>
-      <Background type="triangles" backgroundColor="black" />
+    <div>
+      <div>
+        {backgroundTypes.map(type => (
+          <button onClick={() => setBackgroundType(type)} key={type}>
+            {type}
+          </button>
+        ))}
+      </div>
+      <div style={{ minHeight: '400px', position: 'relative' }}>
+        <Background type={backgroundType} backgroundColor="black" />
+      </div>
     </div>
   );
 };
